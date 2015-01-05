1c4ba37f8e2c02bad777bd4a150080183ea5a0ee
Chris Dillon <squarism@gmail.com>
2015-01-05 13:00:34 -0800

    starfield fade in

---
+++ src/actors/starfield_actor.rb
@@ -0,0 +1,10 @@
+define_actor :starfield do
+
+  has_behaviors do
+    graphical scale: 5
+    positioned
+    layered
+    fading
+  end
+
+end
--- src/stages/work_stage.rb
+++ src/stages/work_stage.rb
@@ -13,7 +13,6 @@ define_stage :work do
 
     timer_manager.add_timer 'sundown', 10 do
       timer_manager.remove_timer 'sundown'
-
       behavior_factory.add_behavior sun, :sliding
       sun.emit :slide, x:60, y:200, time: 25000, style: Tween::Linear
     end
@@ -24,14 +23,18 @@ define_stage :work do
       sun.emit :fade_out, 25000
     end
 
+    timer_manager.add_timer 'nightfade', 20000 do
+      timer_manager.remove_timer 'nightfade'
+      night_sky = create_actor :starfield, x:320, y:240, layer: -1
+      night_sky.alpha = 0
+      night_sky.emit :fade_in, 10000
+    end
+
     timer_manager.add_timer 'bring_in_night', 2000 do
       timer_manager.remove_timer 'bring_in_night'
-
       day_sky.emit :fade_out, 10000
-
       desk_night = create_actor :desk_night, x:330, y:226, layer:4
       library_night = create_actor :library_night, x:320, y:240, layer:1
-
       desk.emit :fade_out, 15000
       library.emit :fade_out, 15000
     end
