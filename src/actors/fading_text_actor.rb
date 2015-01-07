9d9b73f28858503124141a09a914598cac7aae9e
Chris Dillon <squarism@gmail.com>
2015-01-07 11:51:00 -0800

    minor: fading text cleanup

--- src/actors/fading_text_actor.rb
+++ src/actors/fading_text_actor.rb
@@ -10,13 +10,13 @@ define_actor :fading_text do
     requires :font_style_factory
 
     setup do
-      actor.has_attributes text:        "",
-                             x:         0,
-                             y:         0,
-                             font_size: Gamebox.configuration.default_font_size,
-                             font_name: Gamebox.configuration.default_font_name,
-                             color:     Color.new(255, 255, 255, 255),
-                             layer:     1
+      actor.has_attributes text:      "",
+                           x:         0,
+                           y:         0,
+                           font_size: Gamebox.configuration.default_font_size,
+                           font_name: Gamebox.configuration.default_font_name,
+                           color:     Color.new(255, 255, 255, 255),
+                           layer:     1
 
       font_style = font_style_factory.build actor.font_name, actor.font_size, actor.color
       actor.has_attributes font_style: font_style
