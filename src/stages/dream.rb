f0b721d57a9e07fe9ee8e528296e7206c607d4bc
Chris Dillon <squarism@gmail.com>
2015-01-09 17:59:08 -0800

    finish rainbow

--- src/stages/dream.rb
+++ src/stages/dream.rb
@@ -34,12 +34,14 @@ define_stage :dream do
     moon   = create_actor :moon, x:160, y:80, layer: 2
     stars  = create_actor :starfield, x:320, y:240, layer: 1
 
+    galaxy_blank = nil
     star_trails = nil
 
     # timing factors
     # TODO rename all these to _t
-    moon_remove_time     = 3 # 30_000
-    star_transition_lead = 5 # 15_000
+    # TODO possibly use Gosu::milliseconds for current time?
+    moon_remove_time     = 35_000
+    star_transition_lead = 15_000
     galaxy_fade_time     =  5_000
 
     # star trails
@@ -50,11 +52,16 @@ define_stage :dream do
     # This is the second set of prisms timing ... I don't want lines to overlap
     prism_intro_delay    = trail_stop_time + 14_000
 
+    rainbow_intro_t     = prism_intro_delay + 12_000
+    rainbow_line_t      = 15_000
+    rainbow_stop_t      = rainbow_intro_t + 60_000
+
+
 
 
     # Scene Events
     # -------------------------------------------------------------------------
-    timer_manager.add_timer 'moon_slide', 0 do
+    timer_manager.add_timer 'moon_slide', 100 do
       timer_manager.remove_timer 'moon_slide'
       behavior_factory.add_behavior moon, :sliding
       moon.emit :slide, x:160, y:-380, time: moon_remove_time, style: Tween::Quad::InOut
@@ -67,10 +74,10 @@ define_stage :dream do
 
     timer_manager.add_timer 'star_switch', star_transition_lead do
       timer_manager.remove_timer 'star_switch'
-      galaxy  = create_actor :starfield_galaxy_blank, x:320, y:240, layer: 0
-      behavior_factory.add_behavior galaxy, :fading
+      galaxy_blank  = create_actor :starfield_galaxy_blank, x:320, y:240, layer: 0
+      behavior_factory.add_behavior galaxy_blank, :fading
       behavior_factory.add_behavior stars, :fading
-      galaxy.emit :fade_in, galaxy_fade_time / 4
+      galaxy_blank.emit :fade_in, galaxy_fade_time / 4
       stars.emit :fade_out, galaxy_fade_time
     end
 
@@ -108,8 +115,47 @@ define_stage :dream do
       emit_prism({ x: 455, y: -110 }, 13_000)
       emit_prism({ x: 705, y: -50 }, 11_500)
       emit_prism({ x: 605, y: -120 }, 18_500)
-      emit_prism({ x: 800, y: -140 }, 4_500)
+      emit_prism({ x: 950, y: -140 }, 4_500)
     end
+
+
+    timer_manager.add_timer 'rainbow_intro', 1000 do # #rainbow_intro_t do
+      timer_manager.remove_timer 'rainbow_intro'
+
+      # behavior_factory.add_behavior galaxy_blank, :fading
+      # galaxy_blank.emit :fade_out, galaxy_fade_time
+
+      butterfly = create_actor :butterfly, x:0, y:280, layer: 1, rotation: 0
+      tween_manager.tween_properties butterfly, {x: 550, y:160}, rainbow_line_t, Tween::Sine::InOut
+
+      timer_manager.add_timer 'rainbow_ne', rainbow_line_t * 1 do
+        tween_manager.tween_properties butterfly, {x: 550, y:-80}, rainbow_line_t, Tween::Sine::InOut
+      end
+
+      timer_manager.add_timer 'rainbow_nw', rainbow_line_t * 2 do
+        tween_manager.tween_properties butterfly, {x: 240, y:90}, rainbow_line_t, Tween::Sine::InOut
+      end
+
+      timer_manager.add_timer 'rainbow_sw', rainbow_line_t * 3 do
+        tween_manager.tween_properties butterfly, {x: 340, y:140}, rainbow_line_t, Tween::Sine::InOut
+      end
+
+      timer_manager.add_timer 'stop_rainbow', rainbow_stop_t do
+        timer_manager.remove_timer 'stop_rainbow'
+        timer_manager.remove_timer 'rainbow_ne'
+        timer_manager.remove_timer 'rainbow_nw'
+        timer_manager.remove_timer 'rainbow_sw'
+        tween_manager.tween_properties butterfly, {x: -1000, y:-1000}, rainbow_line_t, Tween::Sine::InOut
+
+        timer_manager.add_timer 'remove_rainbow', rainbow_line_t do
+          timer_manager.remove_timer 'remove_rainbow'
+        end
+      end
+
+    end
+
+    # TODO: wakeup sequence
+
 
 
 
--- src/stages/test.rb
+++ src/stages/test.rb
@@ -3,30 +3,7 @@ define_stage :test do
 
   curtain_up do |*args|
 
-    rainbow_line_t      = 15_000
-    rainbow_line_cycles = 4
 
-    butterfly = create_actor :butterfly, x:150, y:150, layer: 9, rotation: 0
-    tween_manager.tween_properties butterfly, {x: 640, y:280}, rainbow_line_t, Tween::Sine::InOut
-
-    timer_manager.add_timer 'rainbow_ne', rainbow_line_t * 1 do
-      tween_manager.tween_properties butterfly, {x: 600, y:-100}, rainbow_line_t, Tween::Sine::InOut
-    end
-
-    timer_manager.add_timer 'rainbow_nw', rainbow_line_t * 2 do
-      tween_manager.tween_properties butterfly, {x: 240, y:90}, rainbow_line_t, Tween::Sine::InOut
-    end
-
-    timer_manager.add_timer 'rainbow_sw', rainbow_line_t * 3 do
-      tween_manager.tween_properties butterfly, {x: 340, y:280}, rainbow_line_t, Tween::Sine::InOut
-    end
-
-
-    timer_manager.add_timer 'stop_rainbow', (rainbow_line_t * (rainbow_line_cycles + 1) * 4) do
-      timer_manager.remove_timer 'rainbow_ne'
-      timer_manager.remove_timer 'rainbow_nw'
-      timer_manager.remove_timer 'rainbow_sw'
-    end
 
   end
 
