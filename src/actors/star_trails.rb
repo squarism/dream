7c88b2ede1db87ced02dff5cca929b070822f373
Chris Dillon <squarism@gmail.com>
2015-01-10 13:34:08 -0800

    Rework dream stage timing.

--- src/actors/star_trails.rb
+++ src/actors/star_trails.rb
@@ -2,7 +2,7 @@ define_actor :star_trails do
   has_attributes view: :graphical_actor_view
 
   has_behaviors do
-    animated frame_update_time: 200, once: false
+    animated frame_update_time: 100, once: false
     graphical scale: 5
     positioned
     layered
--- src/stages/dream.rb
+++ src/stages/dream.rb
@@ -10,7 +10,7 @@ def create_prism_path origin
   path.merge x2: x2, y2: y2
 end
 
-def emit_prism(origin, time=8_000)
+def emit_prism(origin, time=4_000)
   # require 'pry'; binding.pry
   path = create_prism_path origin
   prism = create_actor :prism, x:path[:x1], y:path[:y1], layer: 2
@@ -39,85 +39,85 @@ define_stage :dream do
 
     # timing factors
     # TODO rename all these to _t
-    # TODO possibly use Gosu::milliseconds for current time?
-    moon_remove_time     = 35_000
-    star_transition_lead = 15_000
-    galaxy_fade_time     =  5_000
-    moon_intro_t          = 22_000
+    moon_outro_t         = 24_000  # 24_000 is perfect
+    moon_outro_speed     = 35_000
+    moon_remove_t        = 24_000
+    star_trans_lead_t    = 22_600 + moon_outro_t
+    galaxy_fade_t        = 14_000
+    star_remove_t        = star_trans_lead_t + galaxy_fade_t
 
     # star trails
-    trail_stop_time      = 12_400 + star_transition_lead
-    trail_sustain_time   =  3_000 + trail_stop_time
-    trail_release_time   =  8_000
+    # star trail animation is 64 frames!  (64 * 200ms = 12800ms) :O
+    trail_stop_t         =  6_200 + star_trans_lead_t
+    trail_sustain_t      =  3_000 + trail_stop_t
+    trail_release_t      =  8_000
 
     # This is the second set of prisms timing ... I don't want lines to overlap
-    prism_intro_delay    = trail_stop_time + 14_000
+    prism_intro_delay    = trail_stop_t + 4_000
 
-    rainbow_intro_t     = prism_intro_delay + 12_000
-    rainbow_line_t      = 15_000
-    rainbow_stop_t      = rainbow_intro_t + 60_000
-
-
+    # the rainbow comes in sort of slowly so we don't need much delay
+    rainbow_intro_t      = prism_intro_delay
+    rainbow_line_t       = 15_000
+    rainbow_stop_t       = 30_000
+    wakeup_t             = 42_000 + moon_outro_t
 
 
     # Scene Events
     # -------------------------------------------------------------------------
-    timer_manager.add_timer 'moon_slide', 100 do
-    timer_manager.add_timer 'moon_slide', moon_intro_t do
+    timer_manager.add_timer 'moon_slide', moon_outro_t do
       timer_manager.remove_timer 'moon_slide'
       behavior_factory.add_behavior moon, :sliding
-      moon.emit :slide, x:160, y:-380, time: moon_remove_time, style: Tween::Quad::InOut
+      moon.emit :slide, x:160, y:-350, time: moon_outro_speed, style: Tween::Quad::InOut
+
+      timer_manager.add_timer 'moon_remove', moon_remove_t do
+        timer_manager.remove_timer 'moon_remove'
+        moon.remove
+      end
     end
 
-    timer_manager.add_timer 'moon_remove', moon_remove_time do
-      timer_manager.remove_timer 'moon_remove'
-      moon.remove
-    end
-
-    timer_manager.add_timer 'star_switch', star_transition_lead do
+    timer_manager.add_timer 'star_switch', star_trans_lead_t do
       timer_manager.remove_timer 'star_switch'
       galaxy_blank  = create_actor :starfield_galaxy_blank, x:320, y:240, layer: 0
       behavior_factory.add_behavior galaxy_blank, :fading
       behavior_factory.add_behavior stars, :fading
-      galaxy_blank.emit :fade_in, galaxy_fade_time / 4
-      stars.emit :fade_out, galaxy_fade_time
+      galaxy_blank.emit :fade_in, galaxy_fade_t / 4
+      stars.emit :fade_out, galaxy_fade_t
     end
 
-    timer_manager.add_timer 'stars_remove', (star_transition_lead + galaxy_fade_time) do
+    timer_manager.add_timer 'stars_remove', star_remove_t do
       timer_manager.remove_timer 'stars_remove'
       stars.remove
     end
 
-    timer_manager.add_timer 'star_trails', star_transition_lead do
+    timer_manager.add_timer 'star_trails', star_trans_lead_t do
       timer_manager.remove_timer 'star_trails'
       star_trails = create_actor :star_trails, x:320, y:320, layer: 1
     end
 
-    timer_manager.add_timer 'trail_stop', trail_stop_time do
+    timer_manager.add_timer 'trail_stop', trail_stop_t do
       timer_manager.remove_timer 'trail_stop'
       star_trails.animating = false
     end
 
-    # star trail animation is 64 frames!  (64 * 200ms = 12800ms) :O
-    timer_manager.add_timer 'trail_fade', trail_sustain_time do
+    timer_manager.add_timer 'trail_fade', trail_sustain_t do
       timer_manager.remove_timer 'trail_fade'
       behavior_factory.add_behavior star_trails, :fading
-      star_trails.emit :fade_out, trail_release_time
+      star_trails.emit :fade_out, trail_release_t
     end
 
-    timer_manager.add_timer 'prism', trail_stop_time do
+    timer_manager.add_timer 'prism', trail_stop_t do
       timer_manager.remove_timer 'prism'
       emit_prism({ x: 450, y: -110 })
-      emit_prism({ x: 700, y: -50 }, 12_500)
-      emit_prism({ x: 600, y: -120 }, 19_500)
+      emit_prism({ x: 700, y: -50 }, 6_500)
+      emit_prism({ x: 600, y: -120 }, 8_500)
     end
 
     timer_manager.add_timer 'moar_prisms', prism_intro_delay do
       timer_manager.remove_timer 'moar_prisms'
-      emit_prism({ x: 455, y: -110 }, 13_000)
-      emit_prism({ x: 705, y: -50 }, 11_500)
-      emit_prism({ x: 605, y: -120 }, 18_500)
-      emit_prism({ x: 950, y: -140 }, 4_500)
+      emit_prism({ x: 455, y: -110 }, 6_000)
+      emit_prism({ x: 705, y: -50 }, 5_500)
+      emit_prism({ x: 605, y: -120 }, 7_500)
+      emit_prism({ x: 950, y: -140 }, 2_500)
     end
 
 
@@ -125,7 +125,7 @@ define_stage :dream do
       timer_manager.remove_timer 'rainbow_intro'
 
       behavior_factory.add_behavior galaxy_blank, :fading
-      galaxy_blank.emit :fade_out, galaxy_fade_time
+      galaxy_blank.emit :fade_out, galaxy_fade_t
 
       butterfly = create_actor :butterfly, x:0, y:280, layer: 1, rotation: 0
       tween_manager.tween_properties butterfly, {x: 550, y:160}, rainbow_line_t, Tween::Sine::InOut
@@ -157,8 +157,10 @@ define_stage :dream do
     end
 
     # TODO: wakeup sequence
-
-
+    timer_manager.add_timer 'wakeup', wakeup_t do
+      timer_manager.remove_timer 'wakeup'
+      moon   = create_actor :moon, x:160, y:80, layer: 2
+    end
 
 
   end
