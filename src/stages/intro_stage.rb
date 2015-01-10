927374e6769e8b6ab108fcac0a709b13e3911f23
Chris Dillon <squarism@gmail.com>
2015-01-10 15:48:56 -0800

    Better intro timing and cleanup.

--- src/stages/intro_stage.rb
+++ src/stages/intro_stage.rb
@@ -17,25 +17,22 @@ define_stage :intro do
     jp = create_actor :fading_text, text: "夢", x: center, y:255, font_size: 24, color: Color.new(255, gray, gray, gray), font_name: i18n_font
 
 
-    fade_out_delay = 1500
-    fade_out_time = 4500
+    fade_out_delay_t = 3500
+    fade_out_t = 4500
 
-    timer_manager.add_timer 'intro', fade_out_delay do
+    timer_manager.add_timer 'intro', fade_out_delay_t do
       timer_manager.remove_timer 'intro'
-      en.emit :fade_out, fade_out_time
-      ko.emit :fade_out, fade_out_time
-      es.emit :fade_out, fade_out_time
-      jp.emit :fade_out, fade_out_time
-      ar.emit :fade_out, fade_out_time
-    end
+      en.emit :fade_out, fade_out_t
+      ko.emit :fade_out, fade_out_t
+      es.emit :fade_out, fade_out_t
+      jp.emit :fade_out, fade_out_t
+      ar.emit :fade_out, fade_out_t
 
-    timer_manager.add_timer 'slide', fade_out_delay do
-      timer_manager.remove_timer 'slide'
       behavior_factory.add_behavior en, :sliding
-      en.emit :slide, x:en.x, y:en.y + 10, time: fade_out_time
+      en.emit :slide, x:en.x, y:en.y + 10, time: fade_out_t
     end
 
-    timer_manager.add_timer 'all_done', fade_out_delay + fade_out_time do
+    timer_manager.add_timer 'all_done', fade_out_delay_t + fade_out_t do
       fire :next_stage
     end
 
